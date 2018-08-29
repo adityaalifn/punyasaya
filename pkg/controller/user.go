dcd1671bffb62a8da5b498427024d45678d6691d
William AD <w.albertusd@gmail.com>
2018-08-29 15:56:35 +0700

    user login

--- pkg/controller/user.go
+++ pkg/controller/user.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"encoding/json"
+	"errors"
 
 	"github.com/adityaalifn/punyasaya/pkg/model"
 	"github.com/tokopedia/tdk/go/app/http"
@@ -15,8 +16,18 @@ type request struct {
 func HandleUserLogin(ctx http.TdkContext) error {
 	body := ctx.Body()
 	request := request{}
-	_ = json.Unmarshal(body, &request)
-	user := model.User{Username: request.Username, Password: request.Password}
+	err := json.Unmarshal(body, &request)
+	if err != nil {
+		return err
+	}
+	if request.Username != "kucing" && request.Password != "lucu" {
+		return ctx.Error(401, errors.New("unauthorized request"))
+	}
+	user := model.User{
+		Username: request.Username,
+		Password: request.Password,
+		Name:     "Kucing mania",
+	}
 	returnObject, _ := json.Marshal(user)
 	ctx.Writer().Write(returnObject)
 	return nil
